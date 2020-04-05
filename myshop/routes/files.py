ad4080661239d315220d703eae14fa2073a404ed
isnandazain <bitartikel@gmail.com>
2020-04-05 14:00:50 +0700

    add files endpoint

---
+++ myshop/routes/files.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify, send_file
+
+from myshop.controllers import player as player_ctrl
+from myshop.exceptions import BadRequest, NotFound
+from myshop.libs.ratelimit import ratelimit
+from configuration import MyShopConfig
+
+
+bp = Blueprint(__name__, "files")
+
+@bp.route("/file/<path:path_file>")
+def get_files(path_file):
+    directory = path_file.split("/")[0]
+    filename = path_file.split("/")[1]
+
+    path_file_send = MyShopConfig.STORAGE_PATH + "/{}/{}".format(directory, filename)
+
+    return send_file(path_file_send, attachment_filename=filename)
