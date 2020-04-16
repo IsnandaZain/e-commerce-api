b73d793405c2fed8c2c046e844b8b7276ab8f572
isnandazain <bitartikel@gmail.com>
2020-04-17 06:41:04 +0700

    update retrieve file

--- myshop/routes/v1/files.py
+++ myshop/routes/v1/files.py
@@ -9,9 +9,12 @@ bp = Blueprint(__name__, "files")
 
 @bp.route("/file/<path:path_file>")
 def get_files(path_file):
-    directory = path_file.split("/")[0]
-    filename = path_file.split("/")[1]
+    str_path_file = ""
+    for i in path_file.split("/"):
+        str_path_file = str_path_file + "/" + i
 
-    path_file_send = MyShopConfig.STORAGE_PATH + "/{}/{}".format(directory, filename)
+    filename = path_file.split("/")[-1]
+
+    path_file_send = MyShopConfig.STORAGE_PATH + str_path_file
 
     return send_file(path_file_send, attachment_filename=filename)
